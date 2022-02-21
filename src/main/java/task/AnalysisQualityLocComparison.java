6f84b99845ee1f60d5a9eb6bbafd01935a9b5d40
Quentin Jaquier <quentin.jaquier@sonarsource.com>
2022-02-21 15:15:14 +0100

    Add java files differences in LOC comparison

--- src/main/java/task/AnalysisQualityLocComparison.java
+++ src/main/java/task/AnalysisQualityLocComparison.java
@@ -21,7 +21,7 @@ public class AnalysisQualityLocComparison {
     FileWriter fileWriter = new FileWriter(OUTPUT_FOLDER + "comparison");
     PrintWriter printWriter = new PrintWriter(fileWriter);
 
-    printWriter.println("Project;Loc in base (CI); Loc in target (autoscan); Additional files in autoscan; additional files in CI");
+    printWriter.println("Project;Loc in base (CI); Loc in target (autoscan); Additional files in autoscan; additional files in CI; Additional files in autoscan (only java); additional files in CI (only java)");
 
     projectsQuality.forEach(p -> printWriter.println(getLocComparison(p)));
 
@@ -45,11 +45,21 @@ public class AnalysisQualityLocComparison {
       .filter(c -> !baseComponents.contains(c))
       .collect(Collectors.toList());
 
-    return String.format("%s;%d;%d;%d;%d",
+    List<Component> missingOnlyJava = missing.stream()
+      .filter(c -> c.getLanguage().equals("java"))
+      .collect(Collectors.toList());
+    List<Component> addedOnlyJava = added.stream()
+      .filter(c -> c.getLanguage().equals("java"))
+      .collect(Collectors.toList());
+
+    return String.format("%s;%d;%d;%d;%d;%d;%d",
       p.getBaseComponent().getKey(),
       baseLoc.getOrDefault("java", 0),
       targetLoc.getOrDefault("java", 0),
       added.size(),
-      missing.size());
+      missing.size(),
+      addedOnlyJava.size(),
+      missingOnlyJava.size()
+    );
   }
 }
