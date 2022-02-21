a5878a3d7d83e4dc1b586ff9104c18964ae1256b
Quentin Jaquier <quentin.jaquier@sonarsource.com>
2022-02-21 14:42:22 +0100

    Improve loc comparison to include number of added/missing files

--- src/main/java/task/AnalysisQualityLocComparison.java
+++ src/main/java/task/AnalysisQualityLocComparison.java
@@ -5,7 +5,10 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import model.Component;
 import model.ProjectAnalysisQuality;
+import model.ProjectAnalysisResult;
 
 public class AnalysisQualityLocComparison {
 
@@ -18,7 +21,7 @@ public class AnalysisQualityLocComparison {
     FileWriter fileWriter = new FileWriter(OUTPUT_FOLDER + "comparison");
     PrintWriter printWriter = new PrintWriter(fileWriter);
 
-    printWriter.println("Project;Loc in base (CI); Loc in target (autoscan)");
+    printWriter.println("Project;Loc in base (CI); Loc in target (autoscan); Additional files in autoscan; additional files in CI");
 
     projectsQuality.forEach(p -> printWriter.println(getLocComparison(p)));
 
@@ -26,11 +29,27 @@ public class AnalysisQualityLocComparison {
   }
 
   private static String getLocComparison(ProjectAnalysisQuality p) {
-    Map<String, Integer> baseLoc = p.getBaseComponentResult().getLocPerLanguages();
-    Map<String, Integer> targetLoc = p.getTargetComponentResult().getLocPerLanguages();
-    return String.format("%s;%d;%d",
+    ProjectAnalysisResult baseComponentResult = p.getBaseComponentResult();
+    ProjectAnalysisResult targetComponentResult = p.getTargetComponentResult();
+
+    Map<String, Integer> baseLoc = baseComponentResult.getLocPerLanguages();
+    Map<String, Integer> targetLoc = targetComponentResult.getLocPerLanguages();
+
+    List<Component> baseComponents = baseComponentResult.getComponents();
+    List<Component> targetComponents = targetComponentResult.getComponents();
+
+    List<Component> missing = baseComponents.stream()
+      .filter(c -> !targetComponents.contains(c))
+      .collect(Collectors.toList());
+    List<Component> added = targetComponents.stream()
+      .filter(c -> !baseComponents.contains(c))
+      .collect(Collectors.toList());
+
+    return String.format("%s;%d;%d;%d;%d",
       p.getBaseComponent().getKey(),
       baseLoc.getOrDefault("java", 0),
-      targetLoc.getOrDefault("java", 0));
+      targetLoc.getOrDefault("java", 0),
+      added.size(),
+      missing.size());
   }
 }
